7d56d2ae0f9cac5750259e32dbec214ba06f10ce
kmetelus <kmetelus@lion.lmu.edu>
2017-11-13 20:37:48 -0800

    added teleporters/its starting to feel like a game

--- Assets/Scripts/SideBumper.cs
+++ Assets/Scripts/SideBumper.cs
@@ -7,14 +7,14 @@ public class SideBumper : MonoBehaviour {
   public GameObject game;
 
 	void OnCollisionEnter(Collision c) {
-		float force = 1.5f;
+		float force = 1.0f;
 		if (c.gameObject.tag == "Ball") {
-			// Debug.Log("Collision with Ball");
-			// Vector3 dir = c.contacts[0].point - transform.position;
-			// Debug.Log("Before: " + dir);
-			// dir = -dir.normalized;
-			// Debug.Log("After: " + dir);
-			// c.rigidbody.AddForce(dir.x * force, 0f, dir.z * force, ForceMode.Impulse);
+			Debug.Log("Collision with Ball");
+			Vector3 dir = transform.position;
+			Debug.Log("Before: " + dir);
+			dir = -dir.normalized;
+			Debug.Log("After: " + dir);
+			c.rigidbody.AddForce(dir.x * force, 0f, dir.z * force, ForceMode.Impulse);
 			game.GetComponent<SuperPinball>().UpdateScore(100);
 		}
 
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Teleport : MonoBehaviour {
+
+  public Transform location;
+  private int triggerCount;
+  private int toUse;
+
+  void Start() {
+    toUse = 1;
+    triggerCount = 0;
+  }
+  private void OnTriggerEnter(Collider o) {
+		if (o.gameObject.CompareTag("Ball")) {
+      triggerCount++;
+			Rigidbody ball = o.gameObject.GetComponent<Rigidbody>();
+      if (triggerCount == toUse) {
+          ball.position = new Vector3(location.position.x, location.position.y + 0.27f, location.position.z);
+          toUse *= 2;
+          Debug.Log("ENTER");
+      }
+		}
+	}
+}
